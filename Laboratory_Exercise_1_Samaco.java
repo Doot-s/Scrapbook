0170c9ec66915778c941877e19e9bc56d923b90b
Doot-s <18lean2005@gmail.com>
2024-11-06 09:07:30 +0800

    Add files via upload

---
+++ Laboratory_Exercise_1_Samaco.java
@@ -0,0 +1,66 @@
+package laboratory_exercise_1_samaco;
+
+import java.util.Scanner;
+        
+public class Laboratory_Exercise_1_Samaco {
+
+    public static void main(String[] args) {
+       
+        
+        
+        Scanner scanner;
+     scanner = new Scanner(System.in);
+     
+     System.out.println("How many cars will you have in the future? ");
+     String quest1 = scanner.nextLine();
+     
+     System.out.println("I will have " + quest1 + " Cars in the future");
+     
+     System.out.print("Whats the place you want to go to in the future? ");
+     String quest2 = scanner.nextLine();
+     
+     System.out.println("The place i want to go to is " + quest2);
+     
+     System.out.println("What is your favorite color? ");
+     String quest3 = scanner.nextLine();
+     
+      System.out.println("My favorite color is " + quest3);
+      
+      System.out.println("How many pets do you have? ");
+      String quest4 = scanner.nextLine();
+      
+       System.out.println("I have " + quest4 + " pets ");
+       
+     System.out.println("What is your favorite food? ");
+     String quest5 = scanner.nextLine();
+     
+      System.out.println("my favorite food is " + quest5); 
+      
+     System.out.println("What would you do if you win the lottery? ");
+     String quest6 = scanner.nextLine();
+     
+      System.out.println("I would " + quest6 );
+     
+     System.out.println("What are you afraid of? ");
+     String quest7 = scanner.nextLine();
+      
+       System.out.println("I am afraid of " + quest7);
+      
+     System.out.println("What is your favorite book? ");
+     String quest8 = scanner.nextLine();
+     
+       System.out.println("My favorite book is " + quest8);
+       
+     System.out.println("What is your favorite movie? ");
+     String quest9 = scanner.nextLine();
+     
+       System.out.println("My favorite movie is " + quest9);
+      
+     System.out.println("What is your favorite music? ");
+     String questfinal = scanner.nextLine();
+     
+      System.out.println("My favorite music is " + questfinal);
+      
+    }
+    
+}
